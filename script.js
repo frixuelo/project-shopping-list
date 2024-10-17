5d80c73cecb7d82b010078b5b012f94440ab5772
frixuelo <pelayogarciamtnz@gmail.com>
2024-10-17 10:41:59 +0200

    App Shopping List - First

---
+++ script.js
@@ -0,0 +1,43 @@
+const inputBox = document.getElementById("input-box");
+const containerList = document.getElementById("container-list");
+function addTask(){
+    if(inputBox.value === ''){
+        alert("Necesito que escribas algo para agregar una tarea.");
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        containerList.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value = "";
+    saveData();
+}
+
+containerList.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentNode.remove();
+        saveData();
+    }
+}, false);
+
+function saveData(){
+    localStorage.setItem("data", containerList.innerHTML);
+}
+
+function showTask(){
+    containerList.innerHTML = localStorage.getItem("data");
+}
+showTask()
+
+inputBox.addEventListener("keypress", function(e) {
+    if (e.key === "Enter") {
+        addTask();
+    }
+});
